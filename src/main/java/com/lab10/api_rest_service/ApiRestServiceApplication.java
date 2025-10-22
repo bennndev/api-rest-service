641d06efcf276a2f77ccd77f81101854d7931512
neB <benjamin.sullca1103@gmail.com>
2025-10-22 16:09:53 -0500

    chore: creación de proyecto

---
+++ src/main/java/com/lab10/api_rest_service/ApiRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lab10.api_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab10/api_rest_service/ApiRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab10.api_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
